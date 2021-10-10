76b467417024514a37963ccea9e24f6320dabac5
dirtygiraffes <jp1535@icloud.com>
2021-10-10 08:51:31 -0500

    added credentials, events controller working
    API call to galore server will not work without decrypting the
    credentials. Requires master key, or a new credentials file.
    
    Basic unorderd list is created for each activity received.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
+require 'net/http'
+
 class EventsController < ApplicationController
   def index
+    uri = URI 'https://api.getgalore-staging.com/v1/events'
+    api_key = Rails.application.credentials.api_key
+
+    response = Net::HTTP.get uri, { 'Api-Key' => api_key }
+
+    events = JSON.parse response
+    render 'events/index', locals: { activities: events['activities'] }
+    #render json: eve
   end
 end
